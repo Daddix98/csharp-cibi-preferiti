0de6de7a719efa08e0b7b0331d7513438e73cb19
Davide Scriboni <davidescriboni94@gmail.com>
2022-04-02 19:51:29 +0200

    BONUS

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -15,15 +15,23 @@ Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
 Le modalità di consegna sono le solite: repo GitHub, commit e push
 */
 
+//Stampo a video il titolo della lista
+
 Console.WriteLine(" I miei cibi preferiti \n");
+
+//Creo array contenente la lista "non in ordine" di classifica dei miei cibi preferiti
 
 string[] cibiPreferiti = { " Nutella", " Pasta al ragù", " Pizza", " Lasagna", " Polpettone" };
 
+//Creo un ciclo for per stampare in video l'elenco dell'array soprastante
 
 for (var i = 0; i < cibiPreferiti.Length; i++)
 {
     Console.WriteLine(cibiPreferiti[i]);
 }
+
+
+//Classifico i cibi contenuti nell'array dal + al - buono secondo i miei gusti e li stampo in video
 
 Console.WriteLine("\n Classifica con cinque posizioni \n ");
 
@@ -33,8 +41,10 @@ Console.WriteLine(" 3° Classificato = " + cibiPreferiti[4]);
 Console.WriteLine(" 4° Classificato = " + cibiPreferiti[3]);
 Console.WriteLine(" 5° Classificato = " + cibiPreferiti[0]);
 
+//Stampo in viedeo il cibo + e - apprezzato secondo classifica 
 
 Console.Write(" \nCIBO PIU' APPREZZATO: " + cibiPreferiti[2] + " \nCIBO MENO APPREZZATO: " + cibiPreferiti[0]);
+
 
 // BONUS:
 /* 
@@ -44,12 +54,45 @@ Attenzione: gestire anche il caso se aveste una classifica con un numero di elem
 In questo caso vanno stampati i 2 elementi in centro alla vostra classifica
 */
 
+// Creo una stringa con i cibi in ordine di classifica per ricavarni successivamente l'elemento miediano 
+
+string[] classifica = { "Pizza", "Pasta al ragù", "Polpettone", "Lasagna", "Nutella" };
 
 // Calcolo i valori centrali (supponendo che l'arrey contenga sempre un totale di elementi disparo)
 
-var cibiMediani = ((cibiPreferiti.Length - 1) / 2) + 1;
+var cibiMediani = ((classifica.Length - 1) / 2) + 1;
+
+//ANALIZZO VARIE METODICHE PER ESERCITARMI 
 
 // Seleziono l'elemento mediano nella posizione (cibiMediani) appena calcolata sottraendo 1
-// sapendo che l'array parte da 0.
+// sapendo che l'array parte da 0. (1° PROVA "IL PIU LOGICO")
 
-Console.WriteLine($"\nL'elemento centrale è: {cibiPreferiti[cibiMediani - 1]}");
+
+Console.WriteLine($"\nL'ELEMENTO CENTRALE E': {classifica[cibiMediani - 1]}");
+
+
+// Prova ciclo for (in questo caso è un passaggio in più ) (2° PROVA UTILIZZANDO IL FOR)
+
+/*
+for (var i = cibiMediani; i < classifica.Length; i++)
+{
+    Console.WriteLine($"\nl'elemento centrale è: { classifica[cibiMediani -1] }");
+    break;
+} 
+*/
+
+//Ricavo la posizione tramite un "IF" (3° PROVA )
+
+/*
+if(cibiMediani <= classifica.Length)
+{
+    Console.WriteLine($"\nL'ELEMENTO CENTRALE E': { classifica[cibiMediani -1] }");
+
+}
+else
+{
+
+}
+*/
+    
+
